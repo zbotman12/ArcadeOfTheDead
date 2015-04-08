5716d3aa2765f608eccad8dadc8ce1891da13c50
Ryan Kline <trk0002@uah.edu>
2015-04-07 21:57:48 -0500

    Revert "stufff?"
    This reverts commit d128073dade9a1dc15de1a8262e9f35ad6ff8470.

--- ArcadeOfTheDead/CollisionFilters.lua
+++ ArcadeOfTheDead/CollisionFilters.lua
@@ -1,5 +1,6 @@
 local CollisionFilters = {};
 
+
 CollisionFilters.brick = { categoryBits=1, maskBits=2};
 CollisionFilters.zombie = { categoryBits=2, maskBits=13};
 CollisionFilters.bullet = { categoryBits=4, maskBits=2};
