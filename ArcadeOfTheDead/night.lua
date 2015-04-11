88ae5e4c5d12e2352b52394514ad5967dc3dff5c
acard <anc0017@uah.edu>
2015-04-11 16:51:11 -0500

    comments

--- ArcadeOfTheDead/night.lua
+++ ArcadeOfTheDead/night.lua
@@ -31,9 +31,6 @@ function scene:show( event )
 	local zombiesPlayerKilled=0;
 	local ticketNum,ticketText,life,zombiesToKill,crossLine;
 	
-	--music
-	--local bgNight = audio.loadStream("sounds/night.mp3")
-	--audio.setMaxVolume(.015, {channel = 1})
 
 	local function newGun (guntype)
 		local gun;
@@ -47,6 +44,7 @@ function scene:show( event )
 	end
 
 	if ( phase == "will" ) then
+		--begin music as scene loads 
 		local bgNight = audio.loadStream("sounds/night.mp3")
 		--sceneGroup:insert(bgNight)
 		audio.setMaxVolume(0.045, {channel = 1})
@@ -153,7 +151,7 @@ function scene:show( event )
 				end
 			end
 		end
-
+		--handler for the game ending
 		function gameOver (event)	
 			Runtime:removeEventListener( "tap", movePlayer );
 			display.remove( heartGroup );							
@@ -172,7 +170,7 @@ function scene:show( event )
 			audio.stop(bgNight)
 			composer.gotoScene( "GameOver", sceneOpt);
 		end	
-
+		--make the zmobie move
 		function moveZombie( zombie )
 			local hits;
 			hits = physics.rayCast( zombie.x, zombie.y, zombie.x, zombie.y+1, "closest" );
@@ -184,7 +182,7 @@ function scene:show( event )
 			    test = transition.to( zombie, {time=self.fT, delay=1, y=zombie.y+100, onComplete=go} );
 			end
 		end
-
+		--make a new zmobie 
 		function spawnZombie( x, y )
 			local zombie = Zombie:new({xPos=x, yPos=y});
 			zombie:spawn(params.spriteSheet);
@@ -194,7 +192,7 @@ function scene:show( event )
 			zombie.shape:addEventListener( "collision", zombieAttackBrick );				
 			moveZombie(zombie.shape);
 		end
-
+		--put random zombie into play
 		function spawnRandomZombie(  )
 			local location = math.random(1,10);
 			if (location == 1) then
@@ -220,7 +218,7 @@ function scene:show( event )
 			end
 			
 		end
-
+		--count zmobies
 		local totalNumZombies = 1;
 		zombiesToKill = 1;
 		function spawnZombieHorde( )
